097e918c09f71061d32688f9312060eba6e2a5c5
xDevG4briel <xXG4briel>
2022-08-26 09:56:41 -0300

    atualizacao operadores

---
+++ 2 - operadores aritméticos, atribuição, comparação/main.py
@@ -0,0 +1,31 @@
+# Operadores aritméticos
+'''
+    + Adição ou sinal positivo
+    - Subtração ou sinal negativo
+    * Multiplicação
+    / Divisão
+    // Divisão inteira
+    % Módulo
+    ** Exponenciação
+'''
+
+
+# Operadores Atribuição
+
+'''
+=	x = 1	x = 1
++=	x += 1	x = x + 1
+-=	x -= 1	x = x - 1
+*=	x *= 1	x = x * 1
+/=	x /= 1	x = x / 1
+%=	x %= 1	x = x % 1
+'''
+
+# Operadores Comparação
+
+# >(Maior que)	Verifica se um valor é maior que outro	x > 5
+# <(Menor que)	Verifica se um valor é menor que outro	x < 5
+# == (Igual a)	Verifica se um valor é igual a outro	x == 5
+# != (Diferente de)	Verifica se um valor é diferente de outro	x != 5
+# >= (Maior ou igual a)	Verifica se um valor é maior ou igual a outro	x >= 5
+# <= (Menor ou igual a)	Verifica se um valor é menor ou igual a outro	x <= 5
---
+++ 3 - operadores lógicos, identidade, associação/main.py
@@ -0,0 +1,21 @@
+# Operadores lógicos
+
+'''
+and	Retorna True se todas as condições forem verdadeiras, caso contrário retorna False	x > 1 and x < 5
+or	Retorna True se uma das condições for verdadeiras, caso contrário retorna False	x > 1 or x < 5
+not	Inverte o resultado: se o resultado da expressão for True, o operador retorna false	not(x > 1 and x < 5)
+'''
+
+# Operadores identidade
+
+'''
+is	Retorna True se as variáveis comparadas forem o mesmo objeto	nome is ‘Marcos’
+is not	Retorna True se as variáveis comparadas não forem o mesmo objeto	x is not ‘Python’
+'''
+
+# Operadores de associação
+
+'''
+in	Retorna True caso o valor seja encontrado na sequência	2 in x
+not in	Retorna True caso o valor não seja encontrado na sequência	2 not in x
+'''
